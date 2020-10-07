95821a48a99cc6a0a4b0a3866ba6631aaa70460b
forexsnyder <forexsnyder@gmail.com>
2020-10-06 20:06:11 -0700

    success

--- heartbeat.py
+++ heartbeat.py
@@ -1,6 +1,29 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+###########################
+# Title: OpsChallenge02
+# Author:Jeff Snyder
+# Date: 06OCT2020
+# Purpose:Create a constant ping to an IP Address
+###########################
 import os, datetime, time
 
+#variables
+now= datetime.datetime.now()
 pop= input("Enter IP Address:")
-interval= input("Enter time interval in seconds:")
-ping= os.system ("ping " + str(pop))
+
+#function
+def pinging():
+    ping= os.system ("ping -c 1 " + str(pop))
+    if ping== 0:
+        print (str(now) + " Network Active to " + pop)
+        print ("Waiting 5 seconds to ping again or press control + C to exit")
+        time.sleep( 5 )
+        pinging()
+    else:
+        print ("There was an error, or network is down")
+        print ("Waiting 5 seconds and then will try again")
+        print ("or press Control + C to exit")
+        time.sleep( 5 )
+        pinging()
+
+pinging()
