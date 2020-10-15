3c3e9c2ff28958d3b45a9adacb37dc132446f72c
forexsnyder <forexsnyder@gmail.com>
2020-10-14 23:24:09 -0700

    challenge

---
+++ Challenge08.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+###########################
+# Title: OpsChallenge02
+# Author:Jeff Snyder
+# Date: 12OCT2020
+# Purpose:Encrypt/Decrypt a File or message 
+###########################
