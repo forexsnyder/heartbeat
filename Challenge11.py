05685320c583f09bfda6e6295f8eecb044d971dc
forexsnyder <forexsnyder@gmail.com>
2020-10-19 15:03:25 -0700

    finished

--- Challenge11.py
+++ Challenge11.py
@@ -12,19 +12,30 @@ from scapy.all import *
 
 
 def scanIP(targetIP,targetPort):
-    port_range= []
-    for x in port_range:
-        if x <= targetPort
+        targetPort= int(targetPort)
+        i = 1
+        while i < targetPort:
             host = targetIP
             src_port = random.randint(1,65535)
-            dst_port =
-    
-        response = sr1(IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=1,verbose=0)
-    print(response)
+            dst_port =i
+            
+            response = sr1(IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=5,verbose=0)
+            if response is None:
+                print("Packet filtered")
+            elif(response.haslayer(TCP)):
+                if(response.getlayer(TCP).flags ==0x12):
+                    send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="R"),timeout=5)
+            elif(response.haslayer(TCP)):
+                if(response.getlayer(TCP).flags ==0x14):
+                    send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="F"),timeout=5)
+                    print("This port is closed")
+
+            i +=1
+            print(response, i)
 
 def main():
     print("Do you want to play a game?")
     targetIP=input("What is the ip address you would like to scan?")
-    targetPort=input("What is the port range you would like to scan?(0-65,535)")
+    targetPort= input("What is the port range you would like to scan?(0-65,535)")
     scanIP(targetIP,targetPort)
 main()
