9d5eb9b2031498f23d3d548ade92ad374b37fdcb
forexsnyder <forexsnyder@gmail.com>
2020-10-19 14:14:35 -0700

    for loop

--- Challenge11.py
+++ Challenge11.py
@@ -7,20 +7,24 @@
 ###########################
 import smtplib
 import random
-import sys
-from scapy.all import ICMP, IP, sr1, TCP
+import sys 
+from scapy.all import *
 
 
-def scanIP(targetIP):
-    host = targetIP
-    port_range= 22
-    src_port = 22
-    dst_port =22
+def scanIP(targetIP,targetPort):
+    port_range= []
+    for x in port_range:
+        if x <= targetPort
+            host = targetIP
+            src_port = random.randint(1,65535)
+            dst_port =
     
-    response = sr1(IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=1,verbose=0)
+        response = sr1(IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=1,verbose=0)
     print(response)
+
 def main():
     print("Do you want to play a game?")
     targetIP=input("What is the ip address you would like to scan?")
-    scanIP(targetIP)
+    targetPort=input("What is the port range you would like to scan?(0-65,535)")
+    scanIP(targetIP,targetPort)
 main()
